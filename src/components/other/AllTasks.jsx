f6f3cc7b37489add7398efa0de0b0670a9a43ec3
Toheed Syed <contacttoheed@gmail.com>
2024-12-30 03:21:40 +0500

    first commit

---
+++ src/components/other/AllTasks.jsx
@@ -0,0 +1,33 @@
+const AllTasks = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 mt-3 rounded h-40 overflow-auto">
+      <div className="bg-red-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Toheed</h2>
+        <h3>Make a Project</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Toheed</h2>
+        <h3>Make a Project</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Toheed</h2>
+        <h3>Make a Project</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Toheed</h2>
+        <h3>Make a Project</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Toheed</h2>
+        <h3>Make a Project</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTasks;
